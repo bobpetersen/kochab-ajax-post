153223f1d1444ec957b2604d78daecd2d41d1baa
bobpetersen <33petersen@gmail.com>
2018-04-19 12:01:37 -0500

    everything works

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,35 @@
 console.log('client.js has loaded');
 
-$(document).ready(onready);
+$(document).ready(onReady);
 
 function onReady() {
-    console.log('jQuery has loaded');    
+  console.log('jQuery has loaded');
+  appendRecords();
 }
+
+function appendRecords() {
+  $.ajax({
+    method: 'GET',
+    url: '/records'
+  }).then(function(response) {
+    response.forEach(function(record) {
+      $('#recordList').append(`<tr>
+                <td>${record.title}</td>
+                <td>${record.artist}</td>
+                <td>${record.year}</td>
+                <td>${record.cost
+                  .toLocaleString('en', {
+                    style: 'currency',
+                    currency: 'USD'
+                  })
+                  .slice(0, -3)}</td>
+            </tr>`);
+    });
+  });
+}
+// for (let index = 0; index < respone.length; index++) {
+//     console.log('response');
+//     $('#recordUp').append(`<li>${response[i]}</li>`);
+//     }
+//   });
+// }
--- server/server.js
+++ server/server.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+//this will live in the database
+const recordCollection = require('./modules/record-collection');
+
 app.use(express.static('server/public'));
+
+// routes (get, post, put delete requests) server/records
+app.get('/records', (req, res) => {
+  res.send(recordCollection);
+});
 
 app.listen(PORT, () => {
     console.log(`up and running on port ${PORT}`);
 });
+
+
 
